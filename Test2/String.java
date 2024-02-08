1381569f19eaeedd60287c5eb57e00273d563d6a
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-08 18:44:22 +0530

    String

---
+++ Test2/String.java
@@ -0,0 +1,28 @@
+package Test2;
+
+
+/// REVERSE THE STRING
+public class String {
+   // public static void main(String[] args) {
+     //   Scanner sc = new Scanner(System.in);
+       // java.lang.String input = sc.nextLine();
+
+        //char[] ch = input.toCharArray();
+
+        //for (int i = ch.length - 1; i >= 0; i--) {
+          //  System.out.print(ch[i]);
+        //}
+
+
+        //MERHOD 2
+       // public static void main(String[] args) {
+         //   String str = "Hello World";
+           // String reversed = new StringBuilder(str).reverse().toString();
+            //System.out.println("Reversed string: " + reversed);
+        //}
+
+    }
+
+
+
+
---
+++ Test2/counter.java
@@ -0,0 +1,16 @@
+package Test2;
+
+public class counter {
+   // public static void main(String[] args) {
+     //   String str = "Hello World";
+       // int vowels = 0, consonants = 0;
+        //str = str.toLowerCase();
+        //for (char ch : str.toCharArray()) {
+          //  if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            //    vowels++;
+           // else if (ch >= 'a' && ch <= 'z')
+             //   consonants++;
+       // }
+       // System.out.println("Vowels: " + vowels);
+        //System.out.println("Consonants: " + consonants);
+    }
