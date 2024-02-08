46bacff43798e46ce55e5890344aac1fc4bdc18d
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-08 18:52:44 +0530

    Create ConvertCase.java

---
+++ Test2/ConvertCase.java
@@ -0,0 +1,9 @@
+public class ConvertCase {
+    public static void main(String[] args) {
+        String str = "Java is fun";
+        String upperCase = str.toUpperCase();
+        String lowerCase = str.toLowerCase();
+        System.out.println("Uppercase: " + upperCase);
+        System.out.println("Lowercase: " + lowerCase);
+    }
+}
